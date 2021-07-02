1692ccdfe592ee90d8d36e866875c715557cbd29
NabeelAhmed22 <69622187+NabeelAhmed22@users.noreply.github.com>
2021-07-02 14:05:22 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -2,8 +2,10 @@
 
 void swap_max(int arr[],int l,int n){
   int largest = -99999,pos,temp;
+  /*for(int i=0;i<l;i++){
+    printf("%i\t",arr[i]);
   }
- 
+  printf("\n");*/
   for(int i=n;i<l;i++){
     if(largest<arr[i]){
       largest = arr[i];
@@ -19,4 +21,7 @@ void ssort(int arr[],int l){
   for(int i=0;i<l;i++){
     swap_max(arr,l,i);
   }
+  /* for(int i=0;i<l;i++){
+    printf("%i\t",arr[i]);
+    }*/
 }
