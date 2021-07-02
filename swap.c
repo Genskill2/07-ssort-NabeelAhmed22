ef4a1e3f0c89e02774b6f43150a0b7237cbf8736
NabeelAhmed22 <69622187+NabeelAhmed22@users.noreply.github.com>
2021-07-02 13:51:23 +0530

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,20 @@
+void swap_max(int arr[],int l,int n){
+  int largest = -99999,pos,temp;
+  }
+  
+  for(int i=n;i<l;i++){
+    if(largest<arr[i]){
+      largest = arr[i];
+      pos=i;
+    }
+  }
+  temp = arr[n];
+  arr[n] = largest;
+  arr[pos] = temp;
+}
+
+void ssort(int arr[],int l){
+  for(int i=0;i<l;i++){
+    swap_max(arr,l,i);
+  }
+}
